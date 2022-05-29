0086e94e95eeb556bab63c86b85cd5acde4d2f83
Ian Connelly <ian@Ian.Connelly MacBookPro>
2022-05-29 11:50:23 +0100

    Adding some basis for running reports in the wizard. Not sure if it will be implemented in the final version

---
+++ pricing_wizard/reports/ian_pricing_report.py
@@ -0,0 +1,11 @@
+# Example class for running a report
+from reports.report_base import report_base
+
+class ian_pricing_report(report_base):
+	# Class constructor
+	def __init__(self):
+		super().__init__()
+
+	# Run the report
+	def run_report(self):
+		pass
---
+++ pricing_wizard/reports/report_base.py
@@ -0,0 +1,46 @@
+# Base class to hold abstract methods for future redshift projects
+from abc import ABC, abstractmethod
+# Prevent override in some cases
+from typing import final
+# Redshift connector for python
+import redshift_connector
+# Password 
+import getpass
+
+class report_base(ABC):
+
+	# Required for derived class
+	@abstractmethod
+	def __init__(self):
+		print ("Using base class")
+		self._conn = None
+		pass
+
+	# Redshift connection
+	@property # Shared property in derived classes
+	@final    # Cannot be changed in derived classes
+	def conn(self):
+		user = input("Enter username : ")
+		pwd  = getpass.getpass()
+		# Make connection (required VPN)
+		self._conn = redshift_connector.connect(
+			host='datawarehouse-production.cpbbk0zu5qod.eu-central-1.redshift.amazonaws.com',
+			port=5439,
+			database='dev',
+			user=user,
+			password=pwd
+			)
+
+	# Cursor from the redshift conection
+	@property # Shared property in derived classes
+	@final    # Cannot be changed in derived classes
+	def cursor(self):
+		if not self._conn:
+			self.conn
+		return self._conn.cursor()
+
+
+	# Required for derived class
+	@abstractmethod
+	def run_report(self):
+		pass
